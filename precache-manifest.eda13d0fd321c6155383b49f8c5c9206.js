a517a752203d9f439201700fa50bc2f6fc9e5870
Bartlomiej Baranski <baranskibartek@interia.pl>
2024-11-24 21:45:58 +0100

    Updates

---
+++ precache-manifest.eda13d0fd321c6155383b49f8c5c9206.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b11b35d43c354a19a1060a49a1fcbc",
+    "url": "/recommendation-system/index.html"
+  },
+  {
+    "revision": "5c5e82cc6b1052aed4f4",
+    "url": "/recommendation-system/static/js/2.1cf17e6b.chunk.js"
+  },
+  {
+    "revision": "8a12df96ba4398cdb782",
+    "url": "/recommendation-system/static/js/main.640fa905.chunk.js"
+  },
+  {
+    "revision": "045099b9ac49d7d92b45",
+    "url": "/recommendation-system/static/js/runtime~main.e9b1a761.js"
+  }
+]);
